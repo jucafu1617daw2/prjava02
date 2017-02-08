e317779dd9b6ed7c0b74ec6463a7861d3fdbfb3f
Excombatient <shoot1994@hotmail.com>
2017-02-08 21:07:16 +0100

    Versió 0.2 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -13,21 +13,21 @@ public class Prjava02 {
 /**
 * @param args the command line arguments
 */
-public static void main(String[] args) {
-System.out.println("versió 0.1 del projecte prjava02");
-try {
-InetAddress adreça = InetAddress.getLocalHost();
-String ipAddr = adreça.getHostAddress();
-String hostname = adreça.getHostName();
-System.out.println("hostname="+hostname);
-System.out.println("Adreça IP: " + ipAddr);
-System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
-System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
-System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
-System.out.println("Versió OS: " + System.getProperty("os.version"));
-}
+    public static void main(String[] args) {
+    System.out.println("versió 0.1 del projecte prjava02");
+    try {
+    InetAddress adreça = InetAddress.getLocalHost();
+    String ipAddr = adreça.getHostAddress();
+    String hostname = adreça.getHostName();
+    System.out.println("hostname="+hostname);
+    System.out.println("Adreça IP: " + ipAddr);
+    System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+    System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+    System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+    System.out.println("Versió OS: " + System.getProperty("os.version"));
+    }
 
-catch (IOException e) {
-}
-}
+    catch (IOException e) {
+    }
+    }
 }
